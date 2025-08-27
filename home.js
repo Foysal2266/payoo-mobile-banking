30ce9f19763ec4c3a615ff163697631e15799e56
Foysal2266 <foys898@gmail.com>
2025-08-27 15:05:13 +0600

    added cashout section

--- home.js
+++ home.js
@@ -26,3 +26,41 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
     
 
 })
+
+// cashoutt
+document.getElementById('withdraw-btn').addEventListener('click',function(e){
+e.preventDefault()
+
+const amount=parseInt(document.getElementById('withdraw-amount').value)
+const availableBalance=parseInt(document.getElementById('available-balance').innerText)
+const agentNumber=document.getElementById('agent-number').value
+const addPin=parseInt(document.getElementById('add-pin').value)
+
+if(agentNumber.length <11){
+    alert('please provide valid number')
+    return;
+}
+if(addPin !==validPin){
+    alert('please provide valid pin number')
+    return;
+}
+
+
+
+
+
+
+const totalNewAvailableBalance=availableBalance-amount
+document.getElementById('available-balance').innerText=totalNewAvailableBalance
+})
+
+// toggling
+
+document.getElementById("add-btn").addEventListener('click',function(){
+    document.getElementById("cashout-parent").style.display = "none"
+    document.getElementById("addmoney-parent").style.display ="block"
+})
+document.getElementById("cashout-btn").addEventListener('click',function(){
+    document.getElementById("addmoney-parent").style.display = "none"
+    document.getElementById("cashout-parent").style.display ="block"
+})
